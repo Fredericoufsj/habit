6cab4be3de47b45c812baa3d5317e1a0fe914f18
Frederico Thomaz dos Santos <frederico.santos@supersim.com.br>
2023-03-23 23:37:03 -0300

    configuração setup tailwind CSS

--- web/src/components/Habit.tsx
+++ web/src/components/Habit.tsx
@@ -2,9 +2,9 @@ interface HabitProps{
     completed: number;
 }
 
-export function Habit(habitProps : HabitProps) {
+export function Habit(props : HabitProps) {
   return(
-<div>
-    {habitProps.completed}
+<div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 flex items-center justify-center">
+    {props.completed}
 </div>);
 }
